6c5872bbd052e746bfef950557b05247d2c0d217
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2023-09-24 20:51:03 -0700

    adding the localstorage save when button is clicked

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,27 @@
  //Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elementsin the html.
 //col-8
+var localeSettings = {};
+dayjs.locale(localeSettings);
 
 
-$ (document).ready(function() {
-var userMsg = localStorage.getItem('savedMsg');
-if(userMsg) {
-  $("#savedInput").val(userMsg);
-}}
+$(function(){
+
+  var currentTime = dayjs().format("H");
+})
+
+
+
+function enterTxt) {
+$(".saveBtn").on('click', function(){
+  var key = $(this).parent().attr('id');
+  var value = $(this).siblings('.description').val();
+  localStorage.setItem(key, value);
+})
+}
 
 
 //refresh and retreive as needed
 
-$('document').ready(function() {
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -19,7 +29,6 @@ $('document').ready(function() {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-
 
 
 
